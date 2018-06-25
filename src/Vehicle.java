9074bbcc63568457ce7e6cbea5e11aac654bf3cf
BhaviniB <lassiebhavini@gmail.com>
2018-06-25 23:33:39 +0530

    const added

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,24 @@
 public class Vehicle {
+
+
+    private String name;
+    private String size;
+    private int currentVelocity;
+    private int currentDirection;
+
+}
+
+public Vehicle(String name, String  size)
+{
+    this.name=name;
+    this.size=size;
+    this.currentDirection=0;
+    this.currentVelocity=0;
+}
+
+public void steer(int direction)
+{
+    this.currentDirection+=direction;
+    System.out.println("Steering at "+currentDirection+"degrees");
+    
 }
