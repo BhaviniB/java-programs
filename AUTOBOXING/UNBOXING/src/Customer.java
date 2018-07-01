e80c5c201fef1a50efdf7047784eb1d272d74e92
BhaviniB <lassiebhavini@gmail.com>
2018-07-01 22:09:49 +0530

    Customer class added.

---
+++ AUTOBOXING/UNBOXING/src/Customer.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Customer {
+private String name;
+private ArrayList<Double>transactions;
+
+    public Customer(String name,double initialAmount) {
+
+        this.name = name;
+        this.transactions=new ArrayList<Double>();
+        addTransactions(initialAmount);
+
+    }
+    public void addTransactions(double amount){
+
+
+
+        this.transactions.add(amount);
+    }
+
+
+
+}
