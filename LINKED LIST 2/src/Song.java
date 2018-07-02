13fa8e393a91e6f75d5b26926e131825d803ad4a
BhaviniB <lassiebhavini@gmail.com>
2018-07-03 01:14:32 +0530

    Song class created

---
+++ LINKED LIST 2/src/Song.java
@@ -0,0 +1,23 @@
+public class Song {
+
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+
+        this.title = title;
+        this.duration = duration;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title+":"+this.duration; //print out the contents of the class's objects
+        
+
+    }
+}
