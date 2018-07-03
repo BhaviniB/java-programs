4d4efe818b220ff37fad368fd7eed8fbbc2c6e17
BhaviniB <lassiebhavini@gmail.com>
2018-07-04 03:57:10 +0530

    interface created

---
+++ interfaces/src/com/company/ITelephone.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface ITelephone{
+    public void powerOn();
+    public void dial(int phoneNumber);
+    public void answer();
+    public boolean callPhone(int phoneNumber);
+    public boolean isRinging();
+       
+
+}
