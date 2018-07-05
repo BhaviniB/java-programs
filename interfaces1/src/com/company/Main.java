cf0833072540bcb3995f3d0e2b0050cb7e3e55cf
BhaviniB <lassiebhavini@gmail.com>
2018-07-05 23:52:17 +0530

    driver program created

--- interfaces1/src/com/company/Main.java
+++ interfaces1/src/com/company/Main.java
@@ -1,8 +1,89 @@
 package com.company;
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Player tim = new Player("Tim", 10, 15);
+        System.out.println(tim.toString());
+        saveObject(tim);
+
+        tim.setHitPoints(8);
+        System.out.println(tim);
+        tim.setWeapon("Stormbringer");
+        saveObject(tim);
+        loadObject(tim);
+        System.out.println(tim);
+
     }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISaveable objectToSave) {
+        for(int i=0; i<objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+    }
+
+    public static void loadObject(ISaveable objectToLoad) {
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
