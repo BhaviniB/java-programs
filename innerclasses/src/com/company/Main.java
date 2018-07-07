7e49f2b80daae05af7f74ef2e73a20a961883004
BhaviniB <lassiebhavini@gmail.com>
2018-07-08 02:01:57 +0530

    main updated

--- innerclasses/src/com/company/Main.java
+++ innerclasses/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Gearbox mcLaren = new Gearbox(6);
+        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+//        Gearbox.Gear second = new Gearbox.Gear(2, 15.4);
+//        Gearbox.Gear third = new mcLaren.Gear(3, 17.8);
+        System.out.println(first.driveSpeed(1000));
+
     }
 }
