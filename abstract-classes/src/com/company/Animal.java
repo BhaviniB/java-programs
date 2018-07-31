30f8a7b6c1913d25118672f8e0d468cccc7fdaae
BhaviniB <lassiebhavini@gmail.com>
2018-07-31 20:49:47 +0530

    abstract clas added

---
+++ abstract-classes/src/com/company/Animal.java
@@ -0,0 +1,15 @@
+ package com.company;
+
+public abstract class Animal {
+    private String name;
+    public Animal() {
+        this.name=name;
+    }
+    public abstract void eat();
+    public abstract void breathe();
+
+    public String getName() {
+        return name;
+    }
+
+}
