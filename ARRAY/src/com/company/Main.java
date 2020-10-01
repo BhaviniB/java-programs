4606fa3da0f1b811a2ba8ee0f4a6fa178b43fd8a
Amit kumar <70947616+Amitkumar233@users.noreply.github.com>
2020-10-01 07:31:31 +0530

    Update Main.java

--- ARRAY/src/com/company/Main.java
+++ ARRAY/src/com/company/Main.java
@@ -60,3 +60,64 @@ public class Main {
         }
 
     }
+
+
+
+
+
+//Question-2  Java Program for removing duplicates from integer array
+
+Read more: https://javarevisited.blogspot.com/2014/01/how-to-remove-duplicates-from-array-java-without-collection-API.html#ixzz6Za5E63bK
+
+
+
+
+import java.util.Arrays;
+/*
+  Java program to remove duplicates from this array. 
+ */
+public class Main {
+
+  
+    public static void main(String args[]) {
+
+        int[][] test = new int[][]{
+            {1, 1, 2, 2, 3, 4, 5},
+            {1, 1, 1, 1, 1, 1, 1},
+            {1, 2, 3, 4, 5, 6, 7},
+            {1, 2, 1, 1, 1, 1, 1},};
+
+        for (int[] input : test) {
+            System.out.println("Array with Duplicates       : " + Arrays.toString(input));
+            System.out.println("After removing duplicates   : " + Arrays.toString(removeDuplicates(input)));
+        }
+    }
+
+    /*
+      Method to remove duplicates from array in Java, without using
+      Collection classes e.g. Set or ArrayList. 
+     */
+    public static int[] removeDuplicates(int[] numbersWithDuplicates) {
+
+        // Sorting array to bring duplicates together      
+        Arrays.sort(numbersWithDuplicates);     
+      
+        int[] result = new int[numbersWithDuplicates.length];
+        int previous = numbersWithDuplicates[0];
+        result[0] = previous;
+
+        for (int i = 1; i < numbersWithDuplicates.length; i++) {
+            int ch = numbersWithDuplicates[i];
+
+            if (previous != ch) {
+                result[i] = ch;
+            }
+            previous = ch;
+        }
+        return result;
+
+    }
+}
+
+
+
